127ced0d92a7974315aa2c178e5f373c84ce6ba9
mesyeti <mesyeti@mesyeti.uk>
2022-05-01 13:26:59 +0100

    update version number

--- src/constants.hh
+++ src/constants.hh
@@ -2,7 +2,7 @@
 
 // app information
 #define APP_NAME    "Yeti's Shell"
-#define APP_VERSION "b0.1.0"
+#define APP_VERSION "b0.2.0"
 
 // important stuff idk what to put in this comment
 #define RETCODE_TRUE  0
